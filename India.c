8ee84758011883a113f1a1bf53b03a5f6e4cdd65
Vedant <vedanttbitw@gmail.com>
2018-11-02 23:58:05 +0530

    these teams are added to staging area

---
+++ India.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+int year,a,b,c,d,e;
+
+
+scanf("%d",&year);
+a=year-2001;
+b=(year-2001)/4;
+c=a+b;
+d=c-7;
+e=c-14;
+if (c<=7)
+
+if ((c>7)&&(c<=14))
+
+if ((c>14)&&(c<=21))
+
+}
